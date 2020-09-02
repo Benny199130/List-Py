84bdca71320e583ca7060ad3f2c832526923066d
Daiki1991 <45293899+Daiki1991@users.noreply.github.com>
2020-09-02 16:27:32 +0200

    List for Python
    this is a small list made with python

---
+++ list.py
@@ -0,0 +1,14 @@
+semester_gradebook = [("politics",80),("latin",96),("dance",97),("architecture",65)]
+
+subjects = ["phisics","computer technology","poetry","history"]
+
+votes = ["98","97","85","88","76"]
+
+   subjects.append("geography")
+      votes.append(100)
+      
+  total_gradebook = list(zip(subjects,votes))
+  total_gradebook.append(("medicine",93))
+  print(total_gradebook)
+  
+  
